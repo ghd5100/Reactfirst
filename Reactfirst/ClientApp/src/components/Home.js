1ba2eb55a1517ceb3797e0de261d3111b4ed717a
ghd5100 <ghd5100@gmail.com>
2022-06-01 17:45:08 +0900

    초기 커밋.

---
+++ Reactfirst/ClientApp/src/components/Home.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from 'react';
+
+export class Home extends Component {
+    static displayName = Home.name;
+
+    render() {
+        return (
+            <div>
+                <h1>안녕하세요.</h1>
+            </div>
+        );
+    }
+}
+ /*컨트롤 k+d -> 한줄짜리 코드로 만들기*/
